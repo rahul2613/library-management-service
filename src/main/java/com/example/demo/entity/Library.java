4235ede553c41a10960cb47d8503d49e3ea4a4a3
rahul-chaudhari <chaudhrirahul1995@gmail.com>
2024-07-10 19:09:47 +0530

    Add Library class as new entity

---
+++ src/main/java/com/example/demo/entity/Library.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Library {
+    private final Map<Book, Integer> books;
+
+    public Library(Map<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public Map<Book, Integer> getBooks() {
+        return books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return books.equals(library.books);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(books);
+    }
+}
